30ca76a68bd788a1363158a4357e3830c897022b
tomdbar <t.d.barrett91@gmail.com>
2018-09-25 13:36:42 +0100

    Move bulk runners into directory.

--- vStirap-runners/vStirapRunner.py
+++ vStirap-runners/vStirapRunner.py
@@ -328,6 +328,351 @@ class vStirap_3lvl_runner():
         except:
             pass
 
+class vStirap_3lvl_pol_decay_runner():
+
+    def __init__(self,
+                 kappa1= 1 * 2.*np.pi,
+                 kappa2= 1 * 2.*np.pi,
+                 gamma = 1 * 2.*np.pi,
+                 branching_ratios = [1/3,1/3,1/3],
+                 R_AL = np.sqrt(1/2) * np.matrix([[1, i],
+                                                  [i, 1]]),
+                 R_CL = np.matrix([[1, 0],
+                                   [0, 1]]),
+                 R_ML = np.sqrt(1/2) * np.matrix([[1, i],
+                                                  [i, 1]]),
+                 deltaP = 0,
+                 deltaL = 0,
+                 deltaC = 0,
+                 atom_states = {"x0":0,"gM":1,"gP":2,"d":3},
+                 cavity_states = [0,1]):
+
+        self.hams = []
+        self.args_hams = dict([('i',i)])
+
+        self.kappa1, self.kappa2 = kappa1, kappa2
+        self.gamma = gamma
+
+        self.branching_ratios=branching_ratios
+
+        self.R_AL = R_AL
+        self.R_CL = R_CL
+        self.R_ML = R_ML
+        self.R_MC = self.R_CL.getH() * R_ML
+
+        def R2args(R):
+            alpha = np.clip(np.abs(R[0, 0]), 0, 1)
+            phi1, phi2 = np.angle(R[0, 0]), np.angle(R[1, 0])
+            beta = np.sqrt(1-alpha**2)
+            return alpha, beta, phi1, phi2
+
+        self.alpha_CL, self.phi1_CL, self.phi2_CL, self.beta_CL = R2args(self.R_CL)
+        self.alpha_ML, self.phi1_ML, self.phi2_ML, self.beta_ML = R2args(self.R_ML)
+        self.alpha_MC, self.phi1_MC, self.phi2_MC, self.beta_MC = R2args(self.R_MC)
+
+        self.__configure_birefringence(self.alpha_CL, self.beta_CL, self.phi1_CL, self.phi2_CL)
+
+        self.deltaP = deltaP
+        self.deltaL = deltaL
+        self.deltaC = deltaC
+
+        self.atom_states = atom_states
+        self.cavity_states = cavity_states
+
+        self.M = len(self.atom_states)
+        self.N = len(self.cavity_states)
+
+        self.ketbras = self.__configure_ketbras()
+
+        self.c_op_list = self.__configure_c_ops(self.alpha_MC, self.phi1_MC, self.phi2_MC, self.beta_MC,
+                                                self.branching_ratios)
+
+    def run(self, amp_drive, len_drive=1, n_steps=201, psi0 = ['gM',0,0]):
+
+        self.psi0 = psi0
+
+        A = amp_drive
+        w = np.pi / len_drive
+
+        t, self.tStep = np.linspace(0, len_drive, n_steps, retstep=True)
+
+        self.H, args_ham = self.__generate_Hamiltonian(A, w)
+        self.args = {**args_ham, **dict([['A',A],['w',w]])}
+
+        def ket(atom, cavH, cavV):
+            return tensor(basis(self.M, self.atom_states[atom]), basis(self.N, cavH), basis(self.N, cavV))
+
+        psi0 = ket(*self.psi0)
+        output = mesolve(self.H, psi0, t, self.c_op_list, [], args=self.args)
+
+        return output
+
+    def __configure_ketbras(self):
+
+        def ket(atom, cavX, cavY):
+            return tensor(basis(self.M, self.atom_states[atom]), basis(self.N, cavX), basis(self.N, cavY))
+
+        def bra(atom, cavX, cavY):
+            return ket(atom, cavX, cavY).dag()
+
+        kets, bras = {}, {}
+        ketbras = {}
+
+        s = [list(self.atom_states), self.cavity_states, self.cavity_states]
+        states = list(map(list, list(product(*s))))
+        for state in states:
+            kets[str(state)] = ket(*state)
+            bras[str(state)] = bra(*state)
+
+        for x in list(map(list, list(product(*[states, states])))):
+            ketbras[str(x)] = ket(*x[0]) * bra(*x[1])
+
+        return ketbras
+
+    def __configure_birefringence(self, alpha_CL, beta_CL, phi1_CL, phi2_CL):
+        self.R_AL = np.sqrt(1 / 2) * np.matrix([[1, i],
+                                                [i, 1]])
+
+        self.R_CL = np.matrix([[alpha_CL * np.exp(i * phi1_CL), -beta_CL * np.exp(-i * phi2_CL)],
+                          [beta_CL * np.exp(i * phi2_CL), alpha_CL * np.exp(-i * phi1_CL)]])
+        self.R_LC = self.R_CL.getH()
+
+        '''
+        Rotation from Atom -> Cavity: R_LA: {|+>,|->} -> {|X>,|Y>}
+        '''
+        self.R_AC = self.R_LC * self.R_AL
+        self.R_CA = self.R_AC.getH()
+
+        '''
+        alpha, phi for writing the atomic basis in terms of the cavity basis
+        '''
+        self.alpha_AC = np.abs(self.R_AC[0, 0])
+        self.beta_AC = np.sqrt(1 - self.alpha_AC ** 2)
+        self.phi1_AC, self.phi2_AC = np.angle(self.R_AC[0, 0]), np.angle(self.R_AC[1, 0])
+
+        self.args_hams.update({"alpha":self.alpha_AC,
+                               "beta":self.beta_AC,
+                               "phi1":self.phi1_AC,
+                               "phi2":self.phi2_AC})
+
+    def __configure_c_ops(self,
+                    alpha_MC, beta_MC, phi1_MC, phi2_MC,
+                    branching_ratios):
+        # Define collapse operators
+        c_op_list = []
+
+        aX = tensor(qeye(self.M), destroy(self.N), qeye(self.N))
+        aY = tensor(qeye(self.M), qeye(self.N), destroy(self.N))
+
+        aM1X = np.conj(np.exp(i * phi1_MC) * alpha_MC) * aX
+        aM1Y = np.conj(np.exp(i * phi2_MC) * beta_MC) * aY
+        aM2X = np.conj(-np.exp(-i * phi2_MC) * beta_MC) * aX
+        aM2Y = np.conj(np.exp(-i * phi1_MC) * alpha_MC) * aY
+
+        for kappa, aMX, aMY in zip([self.kappa1, self.kappa2], [aM1X, aM2X], [aM1Y, aM2Y]):
+            c_op_list.append(2 * kappa * lindblad_dissipator(aMX))
+            c_op_list.append(2 * kappa * lindblad_dissipator(aMY))
+            c_op_list.append([2 * kappa * (sprepost(aMY, aMX.dag())
+                                           - 0.5 * spost(aMX.dag() * aMY)
+                                           - 0.5 * spre(aMX.dag() * aMY)),
+                              'np.exp(i*deltaP*t)'])
+            c_op_list.append([2 * kappa * (sprepost(aMX, aMY.dag())
+                                           - 0.5 * spost(aMY.dag() * aMX)
+                                           - 0.5 * spre(aMY.dag() * aMX)),
+                              'np.exp(-i*deltaP*t)'])
+
+        self.args_hams.update({"deltaP":self.deltaP})
+
+        spontEmmChannels = [
+            # |F',mF'> --> |F=1,mF=-1>
+            ('gM', 'x0', branching_ratios[0]),
+            ('gP', 'x0', branching_ratios[1]),
+            ('d', 'x0', branching_ratios[2])
+        ]
+
+        spontDecayOps = []
+
+        for x in spontEmmChannels:
+            try:
+                spontDecayOps.append(x[2] * np.sqrt(2 * self.gamma) *
+                                     tensor(
+                                         basis(self.M, self.atom_states[x[0]]) * basis(self.M, self.atom_states[x[1]]).dag(), qeye(self.N),
+                                         qeye(self.N)))
+            except KeyError:
+                pass
+
+        c_op_list += spontDecayOps
+
+        self.sigma_spontDecayOp = sum([x.dag() * x for x in spontDecayOps])
+
+        return c_op_list
+
+    def __check_coupling(self,g,x):
+        if not g in self.atom_states or not x in self.atom_states:
+            raise Exception("Invalid atom state entered.\nConfigured states are",
+                            print(self.atom_states.values()))
+
+    '''
+    Create a laser coupling.
+
+    Parameters:
+        Omega - The peak rabi frequency of the pump pulse.
+        g - The ground atomic atomic level.
+        x - The excited atomic level.
+        omegaL - The detuning of the pump laser.
+        deltaM - The angular momentum change from g --> x.  This is ignored but included
+                 for consistancy with the cavityCoupling function.
+        args_list - A dictionary of arguments for the qutip simulation.
+        pulseShape - The shape of the pump pulse.
+
+    Returns:
+        (List of cython-ready Hamiltonian terms,
+         args_list with relevant parameters added)
+    '''
+    def add_laser_coupling(self, Omega=1*2.*np.pi, g='gM', x='x0', omegaL=None, pulse_length=1):
+
+        self.__check_coupling(g,x)
+
+        omegaL = self.deltaL if omegaL is None else omegaL
+
+        omegaL_lab = 'omegaL_{0}{1}'.format(g, x)
+        pulse_shape = 'np.piecewise(t, [t<pulse_length_{0}{1}], [np.sin(w_stirap_{0}{1}*t)**2,0])'.format(g, x)
+
+        self.args_hams[omegaL_lab] = omegaL
+        self.args_hams.update([('w_stirap_{0}{1}'.format(g, x), np.pi/pulse_length),
+                               ('pulse_length_{0}{1}'.format(g, x), pulse_length)])
+
+        def kb(a, b):
+            return self.ketbras[str([a, b])]
+
+        self.hams.append( [
+         [ -(Omega/2)*(
+                ( kb([g,0,0],[x,0,0]) + kb([g,0,1],[x,0,1]) +
+                  kb([g,1,0],[x,1,0]) + kb([g,1,1],[x,1,1]) ) +
+                ( kb([x,0,0],[g,0,0]) + kb([x,0,1],[g,0,1]) +
+                  kb([x,1,0],[g,1,0]) + kb([x,1,1],[g,1,1]) )
+        ),'{0} * np.cos({1}*t)'.format(pulse_shape,omegaL_lab)],
+        [ i*(Omega/2)*(
+                ( kb([x,0,0],[g,0,0]) + kb([x,0,1],[g,0,1]) +
+                  kb([x,1,0],[g,1,0]) + kb([x,1,1],[g,1,1]) ) -
+                ( kb([g,0,0],[x,0,0]) - kb([g,0,1],[x,0,1]) -
+                  kb([g,1,0],[x,1,0]) - kb([g,1,1],[x,1,1]) )
+        ),'{0} * np.sin({1}*t)'.format(pulse_shape,omegaL_lab)]
+         ])
+
+    '''
+    Create a cavity coupling.
+
+    Parameters:
+        g0 - The atom-cavity coupling rate.
+        g - The ground atomic atomic level.
+        x - The excited atomic level.
+        omegaC - The detuning of the cavity resonance.
+        deltaM - The angular momentum change from g --> x.
+        args_list - A dictionary of arguments for the qutip simulation.
+
+    Returns:
+        (List of cython-ready Hamiltonian terms,
+         args_list with relevant parameters added)
+    '''
+    def add_cavity_coupling(self, g0=1*2.*np.pi, g='gP', x='x0', omegaC=None, deltaM=1):
+        self.__check_coupling(g, x)
+
+        omegaC = self.deltaC if omegaC is None else omegaC
+
+        omegaC_X = omegaC + self.deltaP / 2
+        omegaC_Y = omegaC - self.deltaP / 2
+        omegaC_X_lab = 'omegaC_X_{0}{1}'.format(g, x)
+        omegaC_Y_lab = 'omegaC_Y_{0}{1}'.format(g, x)
+
+        self.args_hams[omegaC_X_lab] = omegaC_X
+        self.args_hams[omegaC_Y_lab] = omegaC_Y
+
+        def kb(a, b):
+            return self.ketbras[str([a, b])]
+
+        if deltaM == 1:
+            H_coupling = [
+                [-g0 * self.alpha_AC * (
+                        kb([g, 1, 0], [x, 0, 0]) + kb([g, 1, 1], [x, 0, 1]) +
+                        kb([x, 0, 0], [g, 1, 0]) + kb([x, 0, 1], [g, 1, 1])
+                ), 'np.cos({0}*t + phi1)'.format(omegaC_X_lab)],
+
+                [-i * g0 * self.alpha_AC * (
+                        kb([g, 1, 0], [x, 0, 0]) + kb([g, 1, 1], [x, 0, 1]) -
+                        kb([x, 0, 0], [g, 1, 0]) - kb([x, 0, 1], [g, 1, 1])
+                ), 'np.sin({0}*t + phi1)'.format(omegaC_X_lab)],
+
+                [-g0 * self.beta_AC * (
+                        kb([g, 0, 1], [x, 0, 0]) + kb([g, 1, 1], [x, 1, 0]) +
+                        kb([x, 0, 0], [g, 0, 1]) + kb([x, 1, 0], [g, 1, 1])
+                ), 'np.cos({0}*t + phi2)'.format(omegaC_Y_lab)],
+
+                [-i * g0 * self.beta_AC * (
+                        kb([g, 0, 1], [x, 0, 0]) + kb([g, 1, 1], [x, 1, 0]) -
+                        kb([x, 0, 0], [g, 0, 1]) - kb([x, 1, 0], [g, 1, 1])
+                ), 'np.sin({0}*t + phi2)'.format(omegaC_Y_lab)]
+            ]
+
+        elif deltaM == -1:
+            H_coupling = [
+                [-g0 * self.alpha_AC * (
+                        kb([g, 0, 1], [x, 0, 0]) + kb([g, 1, 1], [x, 1, 0]) +
+                        kb([x, 0, 0], [g, 0, 1]) + kb([x, 1, 0], [g, 1, 1])
+                ), 'np.cos({0}*t - phi1)'.format(omegaC_Y_lab)],
+
+                [-i * g0 * self.alpha_AC * (
+                        kb([g, 0, 1], [x, 0, 0]) + kb([g, 1, 1], [x, 1, 0]) -
+                        kb([x, 0, 0], [g, 0, 1]) - kb([x, 1, 0], [g, 1, 1])
+                ), 'np.sin({0}*t - phi1)'.format(omegaC_Y_lab)],
+
+                [g0 * self.beta_AC * (
+                        kb([g, 1, 0], [x, 0, 0]) + kb([g, 1, 1], [x, 0, 1]) +
+                        kb([x, 0, 0], [g, 1, 0]) + kb([x, 0, 1], [g, 1, 1])
+                ), 'np.cos({0}*t - phi2)'.format(omegaC_X_lab)],
+
+                [i * g0 * self.beta_AC * (
+                        kb([g, 1, 0], [x, 0, 0]) + kb([g, 1, 1], [x, 0, 1]) -
+                        kb([x, 0, 0], [g, 1, 0]) - kb([x, 0, 1], [g, 1, 1])
+                ), 'np.sin({0}*t - phi2)'.format(omegaC_X_lab)]
+            ]
+
+        else:
+            raise Exception("deltaM must be +/-1")
+
+        self.hams.append(H_coupling)
+
+    def __generate_hamiltonian(self, ham_name='H_Stirap'):
+        rhs_generate(self.hams, self.c_op_list, args=self.args_hams, name=ham_name, cleanup=False)
+        self.compiled_ham_name = ham_name
+
+    def run(self, psi0=['gM',0,0], t_length=1.2, n_steps=201, ham_name='H_Stirap'):
+
+        t, t_step = np.linspace(0, t_length, n_steps, retstep=True)
+
+        self.hams = list(chain(*self.hams))
+
+        if ham_name!=None:
+            self.__generate_hamiltonian(ham_name)
+            opts = Options(rhs_reuse=False, rhs_filename=ham_name)
+        else:
+            opts = Options()
+
+        def ket(atom, cavX, cavY):
+            return tensor(basis(self.M, self.atom_states[atom]), basis(self.N, cavX), basis(self.N, cavY))
+
+        psi0 = ket(*psi0)
+
+        return mesolve(self.hams, psi0, t, self.c_op_list, [], args=self.args_hams, options=opts)
+
+    def cleanup(self):
+        try:
+            os.remove(self.compiled_ham_name + ".pyx")
+            print("Removed ", self.compiled_ham_name + ".pyx")
+        except:
+            pass
+
+
 
 if __name__=='__main__':
 
