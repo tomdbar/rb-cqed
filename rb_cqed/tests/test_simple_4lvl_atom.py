eb554753410cc7f22cfbf6cf023197e2949985f9
tomdbar <t.d.barrett91@gmail.com>
2019-01-03 18:02:39 +0000

    Add tests for laser-driven rabi oscillations.

--- rb_cqed/tests/test_simple_4lvl_atom.py
+++ rb_cqed/tests/test_simple_4lvl_atom.py
@@ -95,3 +95,81 @@ class TestVacuumRabiOscillations(TestCase):
         for meas, exp in zip([pop_gP, pop_g, pop_gM, pop_x], [exp_pop_gP, exp_pop_g, exp_pop_gM, exp_pop_x]):
             for a, b in zip(meas, exp):
                 self.assertAlmostEqual(a, b, delta=abs_tol)
+
+class TestRabiOscillations(TestCase):
+    '''
+    TestRabiOscillations
+    '''
+
+    @classmethod
+    def setUpClass(cls):
+        cls.atom4lvl_default = Atom4lvl(gamma=0)
+
+        cls.cav_default = Cavity(g=5 * 2. * np.pi,
+                                 kappa=0. * 2. * np.pi)
+
+
+        # Our pulse will be on for 0.75us and then switch off.
+        # Note that couple_off_resonance=False means we only consider the 'gM' <-> 'x' transition coupled.
+        length_pulse = 0.75
+        cls.laser_coupling_default = LaserCoupling(omega0=5 * 2 * np.pi,
+                                                   g='gM', x='x',
+                                                   deltaL=0 * 2 * np.pi,
+                                                   deltaM=[1, -1],
+                                                   pulse_shape='np.piecewise(t, [t<length_pulse], [1,0])',
+                                                   args_ham={"length_pulse": length_pulse},
+                                                   couple_off_resonance=False)
+
+        cls.psi0 = ['gM', 0]
+        cls.t_length = 1
+        cls.n_steps = 501
+
+    def setUp(self):
+        self.atom4lvl = copy.copy(self.atom4lvl_default)
+        self.cav = copy.copy(self.cav_default)
+        self.laser_coupling = copy.copy(self.laser_coupling_default)
+
+    def _run_experiment(self):
+        return ExperimentalRunner(
+            atom=self.atom4lvl,
+            cavity=self.cav,
+            laser_couplings=self.laser_coupling,
+            cavity_couplings=[],
+            verbose=True
+        ).run(self.psi0, self.t_length, self.n_steps)
+
+    def test_rabi_oscillations(self):
+        results = self._run_experiment()
+        pop_gM, pop_x = results.get_atomic_population(['gM', 'x'])[:,
+                        [0, 5, 10, 15, 20, 25, 50, 75, 100, 200, 300, 400, 500]]
+
+        exp_pop_gM = np.array([1., 0.97553, 0.90451, 0.79389, 0.65451, 0.5,
+                               0., 0.5, 1., 0.99999, 0.99998, 0.49998,
+                               0.49998])
+
+        for meas, exp in zip([pop_gM, pop_x], [exp_pop_gM, 1 - exp_pop_gM]):
+            for a, b in zip(meas, exp):
+                self.assertAlmostEqual(a, b, delta=abs_tol)
+
+    def test_rabi_oscillations_with_off_resonance_couplings(self):
+        self.laser_coupling.couple_off_resonance=True
+
+        results = self._run_experiment()
+
+        pop_gP, pop_g, pop_gM, pop_x = results.get_atomic_population(['gP', 'g', 'gM', 'x'])[:,
+                                       [0, 5, 10, 15, 20, 25, 50, 75, 100, 200, 300, 400, 500]]
+
+        exp_pop_gP = np.array([0., 0.00015, 0.00236, 0.01145, 0.03413, 0.07728,
+                               0.64457, 0.98198, 0.40085, 0.86324, 0.01915, 0.6236, 0.6236])
+        exp_pop_g = np.array([0., 0., 0., 0., 0., 0.,
+                              0., 0., 0., 0., 0., 0., 0.])
+        exp_pop_gM = np.array([1., 0.97558, 0.90527, 0.79748, 0.66465, 0.5213,
+                               0.03887, 0.00008, 0.1346, 0.00502, 0.74242, 0.04424,
+                               0.04424])
+        exp_pop_x = np.array([0., 0.02427, 0.09237, 0.19108, 0.30122, 0.40142,
+                              0.31657, 0.01794, 0.46455, 0.13174, 0.23843, 0.33216,
+                              0.33216])
+
+        for meas, exp in zip([pop_gP, pop_g, pop_gM, pop_x], [exp_pop_gP, exp_pop_g, exp_pop_gM, exp_pop_x]):
+            for a, b in zip(meas, exp):
+                self.assertAlmostEqual(a, b, delta=abs_tol)
