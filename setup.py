df975dc6b23729ad678795c2953bca088138c91f
tomdbar <t.d.barrett91@gmail.com>
2019-08-27 20:23:10 +0100

    Setup and LICENSE file.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='rb_cqed',
       version='0.1',
@@ -7,16 +10,15 @@ setup(name='rb_cqed',
       author='Tom Barrett',
       author_email='t.d.barrett91@gmail.com',
       license='MIT',
-      packages=['rb_cqed', 'rb_cqed/qutip_patches', 'rb_cqed/tests', 'rb_cqed/atom87rb_params'],
-      setup_requires=[
-          'qutip>=4.3.1',
-          'dataclasses>=0.6',
-          'seaborn>=0.8'
-      ],
+      # packages=['rb_cqed', 'rb_cqed/qutip_patches', 'rb_cqed/tests', 'rb_cqed/atom87rb_params'],
+      packages=find_packages(),
       install_requires=[
+          'scipy>=1.0',
+          'cython>=0.21',
           'qutip>=4.3.1',
           'dataclasses>=0.6',
-          'seaborn>=0.8'
+          'seaborn>=0.8',
+          'jupyter'
       ],
       test_suite='nose.collector',
       tests_require=['nose'],
